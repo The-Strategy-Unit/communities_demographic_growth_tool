6cb7b039fabbce84dac518847a306d7f935e99af
Fran Barton <francis.barton@nhs.net>
2025-03-05 22:56:46 +0000

    Adjust groupings in age group functions

--- R/age_groups.R
+++ R/age_groups.R
@@ -5,9 +5,7 @@ add_age_groups <- function(.data) {
       age_group_cat = dplyr::case_when(
         age_int == 0L ~ "0",
         age_int %in% seq(1L, 4L) ~ "1-4",
-        age_int %in% seq(5L, 9L) ~ "5-9",
-        age_int %in% seq(10L, 15L) ~ "10-15",
-        age_int %in% seq(16L, 17L) ~ "16-17",
+        age_int %in% seq(5L, 17L) ~ "5-17",
         age_int %in% seq(18L, 34L) ~ "18-34",
         age_int %in% seq(35L, 49L) ~ "35-49",
         age_int %in% seq(50L, 64L) ~ "50-64",
@@ -15,7 +13,7 @@ add_age_groups <- function(.data) {
         age_int %in% seq(75L, 84L) ~ "75-84",
         .default = "85+"
       ),
-      dplyr::across(.data$age_group_cat, forcats::fct_inorder),
+      across(age_group_cat, forcats::fct_inorder),
       .keep = "unused"
     )
 }
@@ -25,12 +23,13 @@ add_broad_age_groups <- function(.data) {
     dplyr::arrange(dplyr::pick("age_int")) |>
     dplyr::mutate(
       broad_age_cat = dplyr::case_when(
-        age_int %in% seq(0L, 15L) ~ "0-15",
-        age_int %in% seq(16L, 49L) ~ "16-49",
-        age_int %in% seq(50L, 74L) ~ "50-74",
-        .default = "75+"
+        age_int %in% seq(0L, 4L) ~ "0-4",
+        age_int %in% seq(5L, 17L) ~ "5-17",
+        age_int %in% seq(18L, 64L) ~ "18-64",
+        age_int %in% seq(65L, 84L) ~ "65-84",
+        .default = "85+"
       ),
-      dplyr::across(broad_age_cat, forcats::fct_inorder),
+      across(broad_age_cat, forcats::fct_inorder),
       .keep = "unused"
     )
 }
