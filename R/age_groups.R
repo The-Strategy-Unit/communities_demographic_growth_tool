36573ea4889ba8db33f61a6b082303a88b3aa804
Fran Barton <francis.barton@nhs.net>
2025-03-06 01:55:11 +0000

    🐛 namespacing functions and quoting variables

--- R/age_groups.R
+++ R/age_groups.R
@@ -3,17 +3,17 @@ add_age_groups <- function(.data) {
     dplyr::arrange(dplyr::pick("age_int")) |>
     dplyr::mutate(
       age_group_cat = dplyr::case_when(
-        age_int == 0L ~ "0",
-        age_int %in% seq(1L, 4L) ~ "1-4",
-        age_int %in% seq(5L, 17L) ~ "5-17",
-        age_int %in% seq(18L, 34L) ~ "18-34",
-        age_int %in% seq(35L, 49L) ~ "35-49",
-        age_int %in% seq(50L, 64L) ~ "50-64",
-        age_int %in% seq(65L, 74L) ~ "65-74",
-        age_int %in% seq(75L, 84L) ~ "75-84",
+        .data[["age_int"]] == 0L ~ "0",
+        .data[["age_int"]] %in% seq(1L, 4L) ~ "1-4",
+        .data[["age_int"]] %in% seq(5L, 17L) ~ "5-17",
+        .data[["age_int"]] %in% seq(18L, 34L) ~ "18-34",
+        .data[["age_int"]] %in% seq(35L, 49L) ~ "35-49",
+        .data[["age_int"]] %in% seq(50L, 64L) ~ "50-64",
+        .data[["age_int"]] %in% seq(65L, 74L) ~ "65-74",
+        .data[["age_int"]] %in% seq(75L, 84L) ~ "75-84",
         .default = "85+"
       ),
-      across(age_group_cat, forcats::fct_inorder),
+      dplyr::across("age_group_cat", forcats::fct_inorder),
       .keep = "unused"
     )
 }
@@ -23,13 +23,13 @@ add_broad_age_groups <- function(.data) {
     dplyr::arrange(dplyr::pick("age_int")) |>
     dplyr::mutate(
       broad_age_cat = dplyr::case_when(
-        age_int %in% seq(0L, 4L) ~ "0-4",
-        age_int %in% seq(5L, 17L) ~ "5-17",
-        age_int %in% seq(18L, 64L) ~ "18-64",
-        age_int %in% seq(65L, 84L) ~ "65-84",
+        .data[["age_int"]] %in% seq(0L, 4L) ~ "0-4",
+        .data[["age_int"]] %in% seq(5L, 17L) ~ "5-17",
+        .data[["age_int"]] %in% seq(18L, 64L) ~ "18-64",
+        .data[["age_int"]] %in% seq(65L, 84L) ~ "65-84",
         .default = "85+"
       ),
-      across(broad_age_cat, forcats::fct_inorder),
+      dplyr::across("broad_age_cat", forcats::fct_inorder),
       .keep = "unused"
     )
 }
