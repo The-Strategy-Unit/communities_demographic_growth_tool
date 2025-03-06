4ef65022e5dc29a3fb9538806331b32eebc51f48
Fran Barton <francis.barton@nhs.net>
2025-03-06 01:06:13 +0000

    ♻️ Add unifying plotting function for main projection charts

--- R/plots.R
+++ R/plots.R
@@ -1,5 +1,11 @@
-plot_national_contacts_by_year <- function() {
-  get_national_contacts() |>
+create_main_projection_chart <- function(.data, label) {
+  conv_lab <- \(x) {
+    paste0(format(x, "%Y"), "/", (as.integer(format(x, "%Y")) + 1) %% 100)
+  }
+  .data |>
+    dplyr::mutate(
+      across("fin_year", \(x) as.Date(sub("_[0-9]{2}$", "-01-01", x)))
+    ) |>
     add_broad_age_groups() |>
     dplyr::summarise(
       dplyr::across("projected_contacts", sum),
@@ -12,44 +18,31 @@ plot_national_contacts_by_year <- function() {
     ) +
     ggplot2::labs(x = NULL, y = NULL) +
     ggplot2::scale_y_continuous(
-      labels = scales::label_number(scale = 1e-6, suffix = "mn"),
-      limits = c(0, 6e7)
+      labels = scales::label_number(scale = 1e-6, suffix = "mn")
     ) +
-    ggplot2::scale_x_discrete(
-      breaks = \(x) x[seq(1, length(x), 5)],
-      labels = \(x) sub("_", "/", x)
+    ggplot2::scale_x_date(
+      breaks = seq.Date(as.Date("2022-01-01"), by = "5 years", length.out = 5),
+      minor_breaks = seq.Date(
+        as.Date("2023-01-01"),
+        as.Date("2041-01-01"),
+        "1 year"
+      ),
+      labels = conv_lab,
+      guide = ggplot2::guide_axis(minor.ticks = TRUE)
     ) +
-    StrategyUnitTheme::scale_colour_su(name = "Age group") +
-    ggplot2::theme(
-      legend.position = "inside",
-      legend.position.inside = c(0.9, 0.6)
+    StrategyUnitTheme::scale_colour_su(
+      name = "Age group",
+      guide = ggplot2::guide_legend(nrow = 1)
     ) +
     su_chart_theme()
 }
 
+plot_national_contacts_by_year <- function() {
+  create_main_projection_chart(get_national_contacts(), "national")
+}
+
 plot_icb_contacts_by_year <- function(icb_data) {
-  icb_data |>
-    add_broad_age_groups() |>
-    dplyr::summarise(
-      dplyr::across("projected_contacts", sum),
-      .by = c("fin_year", "broad_age_cat")
-    ) |>
-    ggplot2::ggplot(ggplot2::aes(fin_year, projected_contacts)) +
-    ggplot2::geom_line(
-      ggplot2::aes(colour = broad_age_cat, group = broad_age_cat),
-      linewidth = 1.8
-    ) +
-    ggplot2::labs(x = NULL, y = NULL) +
-    ggplot2::scale_y_continuous(
-      labels = scales::label_number(scale = 1e-3, suffix = "k"),
-      limits = c(0, NA)
-    ) +
-    ggplot2::scale_x_discrete(
-      breaks = \(x) x[seq(1, length(x), 5)],
-      labels = \(x) sub("_", "/", x)
-    ) +
-    StrategyUnitTheme::scale_colour_su(name = "Age group") +
-    su_chart_theme()
+  create_main_projection_chart(icb_data[["data"]][[1]], icb_data[["icb22nm"]])
 }
 
 plot_percent_change_by_age <- function(icb_data) {
