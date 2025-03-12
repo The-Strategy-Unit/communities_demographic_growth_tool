a6377754581ad9983b986d681eb0b3285da2a070
Rhian Davies <thetrianglegirl@gmail.com>
2025-03-12 17:04:43 +0000

    ♻️ Split out national and ICB Fixes #37

---
+++ R/module_national.r
@@ -0,0 +1,34 @@
+# National module
+
+# UI
+nationalUI <- function(id) {
+  ns <- shiny::NS(id)
+
+  bslib::page_fillable(
+    bslib::layout_column_wrap(
+      width = 0.5,
+      height = 300,
+      bslib::card(
+        full_screen = TRUE,
+        bslib::card_header(
+          "Projected contacts (national) by broad age group, 2022/23 - 2042/43"
+        ),
+        shiny::plotOutput(ns("national_contacts_by_year")),
+      )
+    )
+  )
+
+}
+
+
+# Server
+
+nationalServer <- function(id) {
+  shiny::moduleServer(id, function(input, output, session) {
+
+    output$national_contacts_by_year <- shiny::renderPlot({
+      plot_national_contacts_by_year()
+    })
+
+  })
+}
