db228202e00a27029f75f52b4246a11c0d519e1e
Rhian Davies <thetrianglegirl@gmail.com>
2025-03-17 15:37:45 +0000

    💄 Use _brand.yml

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,7 +2,7 @@ app_ui <- function() {
   bslib::page_navbar(
     title = "Communities demographic growth tool",
     lang = "en",
-    theme = bslib::bs_theme(bootswatch = "sketchy"),
+    theme = bslib::bs_theme(brand = TRUE),
     bslib::nav_panel(title = "National", national_ui("national")),
     bslib::nav_panel(title = "ICB level", icb_ui("icb")),
     bslib::nav_panel(title = "About", shiny::p("Third page content."))
