14cb6f855aa3b1c2f8f0e69ed32fd236305999c5
Fran Barton <francis.barton@nhs.net>
2025-03-18 13:53:58 +0000

    🐛 Fix issue with ICB projected populations

--- data-raw/04_contacts_data.R
+++ data-raw/04_contacts_data.R
@@ -88,12 +88,8 @@ join_popn_proj_data <- function(dat, nat = FALSE, pfp = popn_fy_projected) {
 
   fy_age_summary <- projected_contacts_by_fy |>
     dplyr::summarise(
-      across(c("fin_year_popn", "projected_contacts"), sum),
+      proj_contacts_by_fy_age = sum(.data[["projected_contacts"]]),
       .by = c(tidyselect::all_of(ncmi), "fin_year", "age_int")
-    ) |>
-    dplyr::rename(
-      proj_popn_by_fy_age = "fin_year_popn",
-      proj_contacts_by_fy_age = "projected_contacts"
     )
 
   projected_contacts_by_fy |>
@@ -116,12 +112,12 @@ join_popn_proj_data <- function(dat, nat = FALSE, pfp = popn_fy_projected) {
     dplyr::arrange(dplyr::pick(c("fin_year", "age_int")))
 }
 
-
+nat_popn_fy_projected <- readr::read_rds("nat_popn_fy_projected.rds")
 nat_projected_contacts_fy <- readr::read_rds("csds_contacts_icb_summary.rds") |>
   dplyr::mutate(
     nat_contacts_missing_icb = sum(dplyr::if_else(
-      is.na(.data$icb22cdh),
-      .data$contacts,
+      is.na(.data[["icb22cdh"]]),
+      .data[["contacts"]],
       0L
     ))
   ) |>
@@ -129,6 +125,13 @@ nat_projected_contacts_fy <- readr::read_rds("csds_contacts_icb_summary.rds") |>
   dplyr::rename_with(\(x) sub("^icb", "nat", x)) |>
   dplyr::relocate("nat_contacts_missing_icb", .after = 1) |>
   tidyr::nest(.by = tidyselect::starts_with("nat_contacts")) |>
+  dplyr::mutate(
+    across("data", \(x) {
+      purrr::map(x, \(x) {
+        dplyr::left_join(x, nat_popn_fy_projected, c("fin_year", "age_int"))
+      })
+    })
+  ) |>
   readr::write_rds("nat_projected_contacts_fy.rds")
 
 
@@ -143,12 +146,25 @@ hoist_cols <- c(
   "icb_contacts_final_count"
 )
 
-
+icb_popn_fy_projected <- readr::read_rds("icb_popn_fy_projected.rds")
 icb_projected_contacts_fy <- readr::read_rds("csds_contacts_icb_summary.rds") |>
   dplyr::filter(!dplyr::if_any("icb22cdh", is.na)) |>
-  tidyr::nest(.by = c(tidyselect::all_of(icb_cols))) |>
+  tidyr::nest(.by = tidyselect::all_of(icb_cols)) |>
   dplyr::mutate(across("data", \(x) purrr::map(x, join_popn_proj_data))) |>
-  tidyr::hoist("data", !!!hoist_cols, .transform = unique)
+  tidyr::hoist("data", !!!hoist_cols, .transform = unique) |>
+  dplyr::left_join(icb_popn_fy_projected, "icb22cdh") |>
+  tidyr::nest(
+    .by = tidyselect::all_of(c(icb_cols, hoist_cols, "data")),
+    .key = "popn_data"
+  ) |>
+  dplyr::mutate(
+    across("data", \(x) {
+      purrr::map2(x, .data[["popn_data"]], \(x, y) {
+        dplyr::left_join(x, y, c("fin_year", "age_int"))
+      })
+    })
+  ) |>
+  dplyr::select(!"popn_data")
 
 
 usethis::use_data(
