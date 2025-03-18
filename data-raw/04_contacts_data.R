09f6e0678474ca512e9701528444aef567eacaeb
Fran Barton <francis.barton@nhs.net>
2025-03-18 10:39:50 +0000

    Split build_datasets into separate files

--- data-raw/04_contacts_data.R
+++ data-raw/04_contacts_data.R
@@ -1,31 +1,5 @@
-# Helper functions --------------------------------------------------------
-
-read_in_jg_data <- function(table_name) {
-  sconn::sc() |>
-    dplyr::tbl(dbplyr::in_catalog("strategyunit", "csds_jg", table_name))
-}
-read_in_reference_data <- function(table_name) {
-  sconn::sc() |>
-    dplyr::tbl(dbplyr::in_catalog("strategyunit", "default", table_name))
-}
-
-# Read in data tables -----------------------------------------------------
-
-# CSDS data as prepared by JG
-# Contains 191mn rows
-# Data for each care contact from 2021-04-01 to 2023-03-31
-# 2022/23 data: 96,647,105 rows
-csds_data_full_valid <- read_in_jg_data("full_contact_based_valid")
-lsoa11_lad18_lookup_eng <- read_in_reference_data("lsoa11_lad18_lookup_eng")
-consistent_submitters <- here::here("consistent_submitters_2022_23.csv") |>
-  readr::read_csv(col_types = "c") |>
-  dplyr::pull("provider_org_id")
-
-icb_cols <- c("icb22cdh", "icb22nm")
-dq_cols <- c("consistent", "attendance_status")
-tt <- "team_type"
-join_cols <- c("lad18cd", "age_int", "gender_cat")
-
+source(here::here("data-raw", "01_data_setup.R"))
+source(here::here("data-raw", "03_popn_projections.R"))
 
 # https://www.datadictionary.nhs.uk/data_elements/attendance_status.html
 # 5 - attended on time
@@ -72,59 +46,9 @@ csds_contacts_2022_23_icb_summary <- csds_data_full_valid |>
   readr::write_rds("csds_contacts_icb_summary.rds")
 
 
-# Population projections data ---------------------------------------------
-
-pps_folder <- "/Volumes/su_data/nhp/population-projections/demographic_data/"
-ppp_location <- paste0(pps_folder, "projection=principal_proj")
-popn_proj_orig <- sconn::sc() |>
-  sparklyr::spark_read_parquet("popn_proj_data", ppp_location)
-
-# 1,305,304 rows
-popn_proj_tidy <- popn_proj_orig |>
-  dplyr::filter(dplyr::if_any("year", \(x) x >= 2022L)) |>
-  dplyr::select(c(
-    cal_yr = "year",
-    lad18cd = "area_code",
-    age_int = "age",
-    gender_cat = "sex",
-    "value"
-  )) |>
-  dplyr::collect() |>
-  dplyr::mutate(
-    dplyr::across("gender_cat", \(x) dplyr::if_else(x == 1L, "Male", "Female")),
-    dplyr::across("gender_cat", as.factor),
-    dplyr::across(c("age_int", "cal_yr"), as.integer),
-    dplyr::across("value", as.numeric)
-  ) |>
-  # This arrange() step is crucial for the calculation of growth coefficients
-  # in future steps, using lead() and first().
-  dplyr::arrange(dplyr::pick("cal_yr")) |>
-  readr::write_rds("popn_proj_tidy.rds")
-
-
-# Calculate growth coefficients per financial year ------------------------
-
-# 1,245,972 rows
-popn_fy_projected <- readr::read_rds("popn_proj_tidy.rds") |>
-  dplyr::mutate(
-    fin_year = paste0(
-      .data[["cal_yr"]],
-      "_",
-      dplyr::lead(.data[["cal_yr"]]) %% 100
-    ),
-    fin_year_popn = (.data[["value"]] * 0.75) +
-      (dplyr::lead(.data[["value"]]) * 0.25),
-    growth_coeff = .data[["fin_year_popn"]] /
-      dplyr::first(.data[["fin_year_popn"]]),
-    .by = tidyselect::all_of(c("lad18cd", "age_int", "gender_cat")),
-    .keep = "unused"
-  ) |>
-  dplyr::filter(!dplyr::if_any("fin_year_popn", is.na))
-
-
 # Calculate projected community contacts ----------------------------------
 
-join_popn_proj_data <- function(dat, nat = FALSE, y = popn_fy_projected) {
+join_popn_proj_data <- function(dat, nat = FALSE, pfp = popn_fy_projected) {
   ncmi <- if (nat) "nat_contacts_missing_icb" else NULL
   join_cols <- c("lad18cd", "age_int", "gender_cat")
   sum_con <- \(x) sum(x[["contacts"]])
@@ -151,12 +75,12 @@ join_popn_proj_data <- function(dat, nat = FALSE, y = popn_fy_projected) {
     ) |>
     dplyr::summarise(
       across("contacts", sum),
-      .by = c(tidyselect::all_of(c(join_cols, ncmi)), "team_type")
+      .by = tidyselect::all_of(c(join_cols, ncmi, "team_type"))
     )
   icb_contacts_filt <- sum_con(dat_filtered)
 
   projected_contacts_by_fy <- dat_filtered |>
-    dplyr::left_join(y, join_cols, relationship = "many-to-many") |>
+    dplyr::left_join(pfp, join_cols, relationship = "many-to-many") |>
     dplyr::mutate(
       projected_contacts = .data[["contacts"]] * .data[["growth_coeff"]],
       .keep = "unused"
@@ -196,7 +120,9 @@ join_popn_proj_data <- function(dat, nat = FALSE, y = popn_fy_projected) {
 nat_projected_contacts_fy <- readr::read_rds("csds_contacts_icb_summary.rds") |>
   dplyr::mutate(
     nat_contacts_missing_icb = sum(dplyr::if_else(
-      is.na(.data$icb22cdh), .data$contacts, 0L
+      is.na(.data$icb22cdh),
+      .data$contacts,
+      0L
     ))
   ) |>
   join_popn_proj_data(nat = TRUE) |>
@@ -224,9 +150,9 @@ icb_projected_contacts_fy <- readr::read_rds("csds_contacts_icb_summary.rds") |>
   dplyr::mutate(across("data", \(x) purrr::map(x, join_popn_proj_data))) |>
   tidyr::hoist("data", !!!hoist_cols, .transform = unique)
 
+
 usethis::use_data(
-  icb_projected_contacts_fy,
   nat_projected_contacts_fy,
-  internal = TRUE,
+  icb_projected_contacts_fy,
   compress = "xz"
 )
