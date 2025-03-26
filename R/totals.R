8d498abd8d210dd65897be2b4528ccc3550436e5
Fran Barton <francis.barton@nhs.net>
2025-03-26 21:36:11 +0000

    🚩 Make totals functions more generalisable

--- R/totals.R
+++ R/totals.R
@@ -1,59 +1,49 @@
-get_total_fy_contacts <- function(dat, fin_year) {
-  dat[["data"]][[1]] |>
-    dplyr::filter(dplyr::if_any("fin_year", \(x) x == .env$fin_year)) |> # nolint object_usage_linter
-    dplyr::pull("projected_contacts") |>
+get_total_fy_count <- function(dat, fy) {
+  count_col <- paste0("projected_", type)
+  dat |>
+    pluck_data() |>
+    dplyr::filter(.data[["fin_year"]] == {{ fy }}) |>
+    dplyr::reframe(across("data", dplyr::bind_rows)) |>
+    purrr::pluck("data", count_col) |>
     sum()
 }
-get_national_total_fy_contacts <- function(fin_year = "2022_23") {
-  get_total_fy_contacts(get_national_data(), fin_year)
+get_icb_total_fy_count <- function(icb_data, fy = "2022_23") {
+  get_total_fy_count(icb_data, fy)
+}
+get_national_total_fy_count <- function(fy = "2022_23") {
+  get_total_fy_count(get_all_national_data(), fy)
 }
 
-get_icb_total_fy_contacts <- function(icb_data, fin_year = "2022_23") {
-  get_total_fy_contacts(icb_data, fin_year)
-}
-
-get_icb_pct_change_total_fy_contacts <- function(
+get_icb_pct_change_total_fy <- function(
   icb_data,
   baseline_year = "2022_23",
   horizon_year = "2042_43"
 ) {
-  b <- get_icb_total_fy_contacts(icb_data, baseline_year)
-  h <- get_icb_total_fy_contacts(icb_data, horizon_year)
+  b <- get_icb_total_fy_count(icb_data, baseline_year)
+  h <- get_icb_total_fy_count(icb_data, horizon_year)
   round((h - b) * 100 / b, 1)
 }
-get_national_pct_change_total_fy_contacts <- function(
+get_national_pct_change_fy <- function(
   baseline_year = "2022_23",
   horizon_year = "2042_43"
 ) {
-  b <- get_national_total_fy_contacts(baseline_year)
-  h <- get_national_total_fy_contacts(horizon_year)
+  nat_data <- get_all_national_data()
+  b <- get_national_total_fy_count(nat_data, baseline_year)
+  h <- get_national_total_fy_count(nat_data, horizon_year)
   round((h - b) * 100 / b, 1)
 }
 
 get_icb_sentence <- function(dat, horizon) {
-  total_baseline <-
-    get_icb_total_fy_contacts(dat, "2022_23") |>
-    round(-3) |>
-    format(big.mark = ",")
-
-  total_horizon <-
-    get_icb_total_fy_contacts(dat, horizon) |>
-    round(-3) |>
-    format(big.mark = ",")
-
-  percent_change <-
-    get_icb_pct_change_total_fy_contacts(
-      dat,
-      baseline_year = "2022_23",
-      horizon_year = horizon
-    )
+  fmt <- \(x) format(round(x, -3), big.mark = ",")
+  bas <- get_icb_total_fy_count(dat, "2022_23")
+  hrz <- get_icb_total_fy_count(dat, horizon)
+  percent_change <- round((hrz - bas) * 100 / bas, 1)
+  horizon <- stringr::str_replace(horizon, '_', '/')
 
   glue::glue(
-    "The total number of contacts for {dat$icb22nm} is
-     {total_baseline}. <br><br>
-    By the year {stringr::str_replace(horizon, '_', '/')}
-     this is predicted to rise to {total_horizon},
-     an increase of {percent_change}%."
+    "The total number of contacts for {dat$icb22nm} is {fmt(bas)}.<br /><br />
+      By the year {horizon} this is predicted to rise to {fmt(hrz)},
+      an increase of {percent_change}%."
   ) |>
     htmltools::HTML()
 }
