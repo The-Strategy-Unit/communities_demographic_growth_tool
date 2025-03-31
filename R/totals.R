5d753f50f4cf95a76d461e63364f5bc0df1158fc
Fran Barton <francis.barton@nhs.net>
2025-03-31 17:38:34 +0100

    ✨ Add extra information to the national chart header sentence

--- R/totals.R
+++ R/totals.R
@@ -19,6 +19,60 @@ get_national_pct_change <- function(measure) {
   round((hrz - bas) * 100 / bas, 1)
 }
 
+get_national_sentence_data <- function(measure = c("Contacts", "Patients")) {
+  dat <- get_all_national_data(measure) |>
+    pluck_data() |>
+    prepare_plot_data(measure)
+
+  all_baseline <- dat |>
+    dplyr::filter(.data$fin_year == "2022_23") |>
+    dplyr::summarise(dplyr::across("projected_count", sum)) |>
+    dplyr::pull("projected_count")
+  baseline_age_group_counts <- dat |>
+    dplyr::filter(.data$fin_year == "2022_23") |>
+    add_broad_age_groups() |>
+    dplyr::summarise(
+      dplyr::across("projected_count", sum),
+      .by = "broad_age_cat"
+    ) |>
+    tibble::deframe()
+  baseline_age_group_props <- baseline_age_group_counts / all_baseline
+
+  all_midpoint <- dat |>
+    dplyr::filter(.data$fin_year == "2032_33") |>
+    dplyr::summarise(dplyr::across("projected_count", sum)) |>
+    dplyr::pull("projected_count")
+  midpoint_age_group_counts <- dat |>
+    dplyr::filter(.data$fin_year == "2032_33") |>
+    add_broad_age_groups() |>
+    dplyr::summarise(
+      dplyr::across("projected_count", sum),
+      .by = "broad_age_cat"
+    ) |>
+    tibble::deframe()
+  midpoint_age_group_props <- midpoint_age_group_counts / all_midpoint
+
+  all_horizon <- dat |>
+    dplyr::filter(.data$fin_year == "2042_43") |>
+    dplyr::summarise(dplyr::across("projected_count", sum)) |>
+    dplyr::pull("projected_count")
+  horizon_age_group_counts <- dat |>
+    dplyr::filter(.data$fin_year == "2042_43") |>
+    add_broad_age_groups() |>
+    dplyr::summarise(
+      dplyr::across("projected_count", sum),
+      .by = "broad_age_cat"
+    ) |>
+    tibble::deframe()
+  horizon_age_group_props <- horizon_age_group_counts / all_horizon
+
+  list(
+    baseline_props = baseline_age_group_props,
+    midpoint_props = midpoint_age_group_props,
+    horizon_props = horizon_age_group_props
+  )
+}
+
 
 get_national_sentence <- function(measure = c("Contacts", "Patients")) {
   fmt <- \(x) format(round(x, -3), big.mark = ",")
@@ -26,12 +80,12 @@ get_national_sentence <- function(measure = c("Contacts", "Patients")) {
   hrz <- get_national_horizon_count(measure)
   percent_change <- round((hrz - bas) * 100 / bas, 1)
 
-  glue::glue(
-    "The total number of {tolower(measure)} for England in the CSDS for
-      2022/23 was {fmt(bas)}.
-      <br /><br />
-      By the year 2042/43 this is projected to rise to {fmt(hrz)},
-      an increase of {percent_change}%."
+  glue::glue_data(
+    # fmt: skip
+    get_national_sentence_data(measure),
+    "The total number of {tolower(measure)} for England in the CSDS for 2022/23 was {fmt(bas)}.<br />
+      By the year 2042/43 this is projected to rise to {fmt(hrz)}, an increase of {percent_change}%.<br /><br />
+      {measure} within the two older age groups, 65-84 and 85+, together accounted for {round(sum(baseline_props[4:5]) * 100, 1)}% of all community services {tolower(measure)} in 2022/23, projected to rise to {round(sum(midpoint_props[4:5]) * 100, 1)}% in 2032/33 and further to {round(sum(horizon_props[4:5]) * 100, 1)}% by 2042/43."
   ) |>
     htmltools::HTML()
 }
