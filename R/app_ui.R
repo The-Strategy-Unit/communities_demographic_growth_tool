5d5d1ac5ec0684aa69eceb4c1d37ed6ed3b3066d
Rhian Davies <thetrianglegirl@gmail.com>
2025-04-01 23:31:19 +0100

    📝 Add methodology and data sources

--- R/app_ui.R
+++ R/app_ui.R
@@ -5,6 +5,31 @@ app_ui <- function() {
     theme = bslib::bs_theme(brand = TRUE),
     bslib::nav_panel(title = "National", national_ui("national")),
     bslib::nav_panel(title = "ICB", icb_ui("icb")),
-    bslib::nav_panel(title = "About", shiny::p("Third page content."))
+    bslib::nav_panel(
+      title = "About",
+      bslib::layout_column_wrap(
+        width = 1 / 3,
+        bslib::card(
+          bslib::card_header("About the tool"),
+          shiny::includeMarkdown("inst/www/about.md")
+        ),
+        bslib::card(
+          bslib::card_header("Community Services Data Set (CSDS)"),
+          shiny::includeMarkdown("inst/www/csds.md")
+        ),
+        bslib::card(
+          bslib::card_header("Provider exclusions"),
+          shiny::includeMarkdown("inst/www/provider-exclusions.md")
+        ),
+        bslib::card(
+          bslib::card_header("Methodology"),
+          shiny::includeMarkdown("inst/www/methodology.md")
+        ),
+        bslib::card(
+          bslib::card_header("Population projections"),
+          shiny::includeMarkdown("inst/www/projections.md")
+        )
+      )
+    )
   )
 }
